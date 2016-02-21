62fe8f73a798cfae277ab4833dcce1db68b954ce
Martin Kiesel <martin.kiesel@gmail.com>
2016-02-21 13:11:28 +0100

    initial commit

---
+++ afj-interpreter/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  afj-interpreter
+//
+//  Created by Martin Kiesel on 17/02/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include <iostream>
+
+void move();
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
